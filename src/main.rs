f27b2fd5eb6b874f2b851c2ad374e923cbb7fde5
Brandon Kauffman <brandon.kauffman@pulsarinformatics.com>
2024-02-21 07:58:49 -0500

    bitwise-and-of-numbers-range

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,19 @@
 pub struct Solution {}
 impl Solution {
-    /// Finds the missing number  in the array by taking the sum of all numbers in the array and
-    /// the sum of all numbers in the range of the array. It then subtracts the two to find the
-    /// missing number.
+    /// Use common left most bits by appending 0.
     ///
-    ///Time Complexity: O(n)
+    ///Time Complexity: O(nlogn)
     ///Space Complexity: O(1)
-    pub fn missing_number(nums: Vec<i32>) -> i32 {
-        let sum: i32 = nums.iter().sum();
-        let n: i32 = nums.len() as i32;
-        n * (n + 1) / 2 - sum
+    pub fn range_bitwise_and(left: i32, right: i32) -> i32 {
+        let mut left = left;
+        let mut right = right;
+        let mut count = 0;
+        while left != right {
+            left >>= 1;
+            right >>= 1;
+            count += 1
+        }
+        left << count
     }
 }
 
@@ -23,10 +27,14 @@ mod tests {
 
     #[test]
     fn test_1() {
-        assert_eq!(2, Solution::missing_number(vec![3, 0, 1]));
+        assert_eq!(0, Solution::range_bitwise_and(0, 0));
     }
     #[test]
     fn test_2() {
-        assert_eq!(2, Solution::missing_number(vec![0, 1]));
+        assert_eq!(4, Solution::range_bitwise_and(5, 7));
+    }
+    #[test]
+    fn test_3() {
+        assert_eq!(0, Solution::range_bitwise_and(0, 214783647));
     }
 }
