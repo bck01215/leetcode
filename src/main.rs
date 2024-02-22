b306f31362fa7692387e1b3be9524dcbbe836596
Brandon Kauffman <brandon.kauffman@pulsarinformatics.com>
2024-02-22 08:58:55 -0500

    find-the-town-judge

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,22 @@
 pub struct Solution {}
 impl Solution {
-    /// Use common left most bits by appending 0.
+    /// Store relationships people and judge and compare to best case.
     ///
-    ///Time Complexity: O(nlogn)
-    ///Space Complexity: O(1)
-    pub fn range_bitwise_and(left: i32, right: i32) -> i32 {
-        let mut left = left;
-        let mut right = right;
-        let mut count = 0;
-        while left != right {
-            left >>= 1;
-            right >>= 1;
-            count += 1
+    ///Time Complexity: O(n)
+    ///Space Complexity: O(n)
+    pub fn find_judge(n: i32, trust: Vec<Vec<i32>>) -> i32 {
+        let mut trust_vec = vec![vec![0; 2]; n as usize];
+        for val in trust {
+            trust_vec[val[0] as usize - 1][0] += 1; // trust given
+            trust_vec[val[1] as usize - 1][1] += 1; // trust received
         }
-        left << count
+
+        for (index, judge_candidate) in trust_vec.iter().enumerate() {
+            if judge_candidate[0] == 0 && judge_candidate[1] == n - 1 {
+                return (index + 1) as i32;
+            }
+        }
+        -1
     }
 }
 
@@ -27,14 +30,17 @@ mod tests {
 
     #[test]
     fn test_1() {
-        assert_eq!(0, Solution::range_bitwise_and(0, 0));
+        assert_eq!(2, Solution::find_judge(2, vec![vec![1, 2]]));
     }
     #[test]
     fn test_2() {
-        assert_eq!(4, Solution::range_bitwise_and(5, 7));
+        assert_eq!(3, Solution::find_judge(3, vec![vec![1, 3], vec![2, 3]]));
     }
     #[test]
     fn test_3() {
-        assert_eq!(0, Solution::range_bitwise_and(0, 214783647));
+        assert_eq!(
+            -1,
+            Solution::find_judge(3, vec![vec![1, 3], vec![2, 3], vec![1, 3]])
+        );
     }
 }
