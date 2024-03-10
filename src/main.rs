6b5dba10c4e43831fc368536b7b360808fb1e443
Brandon Kauffman <brandon.kauffman@pulsarinformatics.com>
2024-03-10 18:27:18 -0400

    find intersection

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,12 @@
-#[derive(Debug, PartialEq, Eq)]
-pub struct TreeNode {
-    pub val: i32,
-    pub left: Option<Rc<RefCell<TreeNode>>>,
-    pub right: Option<Rc<RefCell<TreeNode>>>,
-}
-
-impl TreeNode {
-    #[inline]
-    pub fn new(val: i32) -> Self {
-        TreeNode {
-            val,
-            left: None,
-            right: None,
-        }
-    }
-}
-
-use std::cell::RefCell;
-use std::rc::Rc;
+use std::collections::HashSet;
+use std::iter::FromIterator;
 pub struct Solution {}
 impl Solution {
-    pub fn find_bottom_left_value(root: Option<Rc<RefCell<TreeNode>>>) -> i32 {
-        let mut level = Vec::from([root.unwrap()]);
-        let mut ans: i32 = 0;
-        while !level.is_empty() {
-            ans = level[0].borrow().val;
-            level = level
-                .into_iter()
-                .flat_map(|n| [n.borrow().left.clone(), n.borrow().right.clone()])
-                .filter_map(|n| n)
-                .collect();
-        }
-        ans
+    pub fn intersection(nums1: Vec<i32>, nums2: Vec<i32>) -> Vec<i32> {
+        HashSet::<i32>::from_iter(nums1)
+            .intersection(&HashSet::<i32>::from_iter(nums2))
+            .map(|&n| n)
+            .collect()
     }
 }
 
@@ -44,54 +19,16 @@ mod tests {
     use super::*;
     #[test]
     fn test_1() {
-        let tree = Some(Rc::new(RefCell::new(TreeNode {
-            val: 2,
-            left: Some(Rc::new(RefCell::new(TreeNode {
-                val: 1,
-                left: None,
-                right: None,
-            }))),
-            right: Some(Rc::new(RefCell::new(TreeNode {
-                val: 3,
-                left: None,
-                right: None,
-            }))),
-        })));
-
-        assert_eq!(1, Solution::find_bottom_left_value(tree));
+        assert_eq!(
+            vec![0],
+            Solution::intersection(vec![0, 1, 1, 0], vec![2, 9, 0])
+        );
     }
     #[test]
     fn test_2() {
-        let tree = Some(Rc::new(RefCell::new(TreeNode {
-            val: 1,
-            left: Some(Rc::new(RefCell::new(TreeNode {
-                val: 2,
-                left: Some(Rc::new(RefCell::new(TreeNode {
-                    val: 4,
-                    left: None,
-                    right: None,
-                }))),
-                right: None,
-            }))),
-            right: Some(Rc::new(RefCell::new(TreeNode {
-                val: 3,
-                left: Some(Rc::new(RefCell::new(TreeNode {
-                    val: 5,
-                    left: Some(Rc::new(RefCell::new(TreeNode {
-                        val: 7,
-                        left: None,
-                        right: None,
-                    }))),
-                    right: None,
-                }))),
-                right: Some(Rc::new(RefCell::new(TreeNode {
-                    val: 6,
-                    left: None,
-                    right: None,
-                }))),
-            }))),
-        })));
-
-        assert_eq!(7, Solution::find_bottom_left_value(tree));
+        assert_eq!(
+            vec![9, 4],
+            Solution::intersection(vec![4, 9, 5], vec![9, 4, 9, 8, 4])
+        );
     }
 }
