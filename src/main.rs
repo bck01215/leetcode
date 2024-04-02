3bf85fdf6082078391c9383d7f11df69a61bb266
Brandon Kauffman <brandon.kauffman@pulsarinformatics.com>
2024-04-02 09:00:41 -0400

    is_isomorphic

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,19 @@
+use std::collections::{HashMap, HashSet};
+
 pub struct Solution {}
 impl Solution {
-    pub fn length_of_last_word(s: String) -> i32 {
-        s.split_whitespace()
-            .collect::<Vec<&str>>()
-            .last()
-            .unwrap_or(&"")
-            .len() as i32
+    pub fn is_isomorphic(s: String, t: String) -> bool {
+        let s = s.into_bytes();
+        let t = t.into_bytes();
+        let mut map: HashMap<u8, u8> = HashMap::with_capacity(s.len());
+        for index in 0..s.len() {
+            let val = map.entry(s[index]).or_insert(t[index]);
+            if *val != t[index] {
+                return false;
+            }
+        }
+
+        map.values().collect::<HashSet<_>>().len() == map.keys().collect::<HashSet<_>>().len()
     }
 }
 
@@ -18,13 +26,16 @@ mod tests {
     use super::*;
     #[test]
     fn test_1() {
-        assert_eq!(5, Solution::length_of_last_word("Hello World".to_string()));
+        assert_eq!(
+            true,
+            Solution::is_isomorphic("egg".to_string(), "add".to_string())
+        );
     }
     #[test]
     fn test_2() {
         assert_eq!(
-            4,
-            Solution::length_of_last_word("   fly me   to   the moon  ".to_string())
+            false,
+            Solution::is_isomorphic("foo".to_string(), "bar".to_string())
         );
     }
 }
