3a0d3c4c79f025b9413f53acaf80267a3f0781f0
Brandon Kauffman <brandon.kauffman@pulsarinformatics.com>
2024-04-04 11:13:49 -0400

    maximum-nesting-depth-of-the-parentheses

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,16 @@
-use std::collections::{HashMap, HashSet};
-
 pub struct Solution {}
 impl Solution {
-    pub fn is_isomorphic(s: String, t: String) -> bool {
-        let s = s.into_bytes();
-        let t = t.into_bytes();
-        let mut map: HashMap<u8, u8> = HashMap::with_capacity(s.len());
-        for index in 0..s.len() {
-            let val = map.entry(s[index]).or_insert(t[index]);
-            if *val != t[index] {
-                return false;
-            }
-        }
-
-        map.values().collect::<HashSet<_>>().len() == map.keys().collect::<HashSet<_>>().len()
+    pub fn max_depth(s: String) -> i32 {
+        s.chars()
+            .fold((0, 0i32), |(max_depth, nesting), c| {
+                // Annotate nesting with i32
+                match c {
+                    '(' => (max_depth.max(nesting + 1), nesting + 1),
+                    ')' => (max_depth, nesting.saturating_sub(1)),
+                    _ => (max_depth, nesting),
+                }
+            })
+            .0
     }
 }
 
@@ -26,16 +23,10 @@ mod tests {
     use super::*;
     #[test]
     fn test_1() {
-        assert_eq!(
-            true,
-            Solution::is_isomorphic("egg".to_string(), "add".to_string())
-        );
+        assert_eq!(3, Solution::max_depth("(1+(2*3)+((8)/4))+1".to_string()));
     }
     #[test]
     fn test_2() {
-        assert_eq!(
-            false,
-            Solution::is_isomorphic("foo".to_string(), "bar".to_string())
-        );
+        assert_eq!(3, Solution::max_depth("(1)+((2))+(((3)))".to_string()));
     }
 }
