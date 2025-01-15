2bb1560a83d572b9034314c832f021c7f4e25323
Brandon Kauffman <bkauffman7@liberty.edu>
2025-01-15 08:13:44 -0500

    Time complexity: O(logn) Space complexity: O(1)

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,19 @@
 pub struct Solution {}
 impl Solution {
-    pub fn max_depth(s: String) -> i32 {
-        s.chars()
-            .fold((0, 0i32), |(max_depth, nesting), c| {
-                // Annotate nesting with i32
-                match c {
-                    '(' => (max_depth.max(nesting + 1), nesting + 1),
-                    ')' => (max_depth, nesting.saturating_sub(1)),
-                    _ => (max_depth, nesting),
-                }
-            })
-            .0
+    pub fn minimize_xor(num1: i32, num2: i32) -> i32 {
+        let mut x = num1;
+        let nbits1 = num1.count_ones();
+        let nbits2 = num2.count_ones();
+        if nbits1 > nbits2 {
+            for _ in nbits2..nbits1 {
+                x &= x - 1;
+            }
+        } else if nbits1 < nbits2 {
+            for _ in nbits1..nbits2 {
+                x |= x + 1;
+            }
+        }
+        x
     }
 }
 
@@ -23,10 +26,10 @@ mod tests {
     use super::*;
     #[test]
     fn test_1() {
-        assert_eq!(3, Solution::max_depth("(1+(2*3)+((8)/4))+1".to_string()));
+        assert_eq!(3, Solution::minimize_xor(3, 5));
     }
     #[test]
     fn test_2() {
-        assert_eq!(3, Solution::max_depth("(1)+((2))+(((3)))".to_string()));
+        assert_eq!(3, Solution::minimize_xor(1, 12));
     }
 }
