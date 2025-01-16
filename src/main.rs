e4df21c61ff4a9572d46e2da55f7d08e70397320
Brandon Kauffman <bkauffman7@liberty.edu>
2025-01-16 09:08:34 -0500

    dp table

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,32 @@
 pub struct Solution {}
 impl Solution {
-    pub fn minimize_xor(num1: i32, num2: i32) -> i32 {
-        let mut x = num1;
-        let nbits1 = num1.count_ones();
-        let nbits2 = num2.count_ones();
-        if nbits1 > nbits2 {
-            for _ in nbits2..nbits1 {
-                x &= x - 1;
-            }
-        } else if nbits1 < nbits2 {
-            for _ in nbits1..nbits2 {
-                x |= x + 1;
+    pub fn stone_game_ii(piles: Vec<i32>) -> i32 {
+        let length = piles.len();
+        let mut dp_table = vec![vec![0; length+1]; length];
+        let mut suffix_sum = vec![0; length];
+        suffix_sum[length - 1] = piles[length - 1];
+
+        for i in (0..length - 1).rev() {
+            suffix_sum[i] = suffix_sum[i + 1] + piles[i];
+        }
+        for i in (0..length).rev() {
+            for m in 1..=length {
+                if i + 2 * m >= length {
+                    dp_table[i][m] = suffix_sum[i];
+                }
+                else {
+                    for x in 1..=2*m{
+                        dp_table[i][m] = dp_table[i][m].max(suffix_sum[i] - dp_table[i + x][m.max(x)]);
+                    }
+                }
+                
             }
         }
-        x
+
+        return dp_table[0][1];
+      
     }
+
 }
 
 fn main() {
@@ -26,10 +38,10 @@ mod tests {
     use super::*;
     #[test]
     fn test_1() {
-        assert_eq!(3, Solution::minimize_xor(3, 5));
+        assert_eq!(10, Solution::stone_game_ii(vec![2,7,9,4,4]));
     }
     #[test]
     fn test_2() {
-        assert_eq!(3, Solution::minimize_xor(1, 12));
+        assert_eq!(104, Solution::stone_game_ii(vec![1,2,3,4,5,100]));
     }
 }
